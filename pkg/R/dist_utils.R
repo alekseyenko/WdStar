d601c102aae6889afa59f0e8069d7f77e24e3c85
koros <ivan.koros2@gmail.com>
2023-08-25 13:01:42 -0400

    add documentation for test_utils and dist_utils

--- pkg/R/dist_utils.R
+++ pkg/R/dist_utils.R
@@ -1,20 +1,84 @@
-is.dist = function(x) any(class(x)=='dist')
+#' Test if an object is of class 'dist'
+#'
+#' This function checks whether the given object belongs to class 'dist'.
+#'
+#' @param x Object to test.
+#'
+#' @return Logical indicating whether the object is a distance matrix.
+#'
+#' @examples
+#' is.dist(as.dist(matrix(1:4, nrow = 2)))
+#' is.dist(matrix(1:4, nrow = 2))
+is.dist = function(x) any(class(x) == 'dist')
 
-dist.sigma2 = function(dm){
+#' Calculate Sigma Squared for Distance Matrix
+#'
+#' This function computes the sigma squared statistic for a given distance matrix.
+#'
+#' @param dm Distance matrix.
+#'
+#' @return Sigma squared value.
+#'
+#' @examples
+#' dm <- as.dist(matrix(runif(100), nrow = 10))
+#' dist.sigma2(dm)
+dist.sigma2 = function(dm) {
   dd = as.matrix(dm)
-  dd[upper.tri(dd)]=0 ##
-  sum(dd^2)/nrow(dd)/(nrow(dd)-1)
+  dd[upper.tri(dd)] = 0
+  sum(dd^2) / nrow(dd) / (nrow(dd) - 1)
 }
 
-dist.ss2 = function(dm2, f){ #dm2 is matrix of square distances; f factor
-  K = sapply(levels(f), function(lev) f==lev)
-  t(K)%*%dm2%*%K/2
+#' Calculate Sum of Squares for Distance Matrix with Factor
+#'
+#' This function calculates the sum of squares for a given distance matrix,
+#' weighted by a factor variable.
+#'
+#' @param dm2 Square distance matrix.
+#' @param f Factor variable for weighting.
+#'
+#' @return Sum of squares matrix.
+#'
+#' @examples
+#' dm2 <- matrix(runif(100), nrow = 10)
+#' f <- factor(c(rep("A", 5), rep("B", 5)))
+#' dist.ss2(dm2, f)
+dist.ss2 = function(dm2, f) {
+  K = sapply(levels(f), function(lev) f == lev)
+  t(K) %*% dm2 %*% K / 2
 }
 
-dist.group.sigma2 = function(dm, f){
-  diag(dist.ss2(as.matrix(dm)^2, f))/table(f)/(table(f)-1)
+#' Calculate Group-wise Sigma Squared
+#'
+#' This function calculates the sigma squared statistic for each group defined
+#' by a factor variable in a given distance matrix.
+#'
+#' @param dm Distance matrix.
+#' @param f Factor variable for group definition.
+#'
+#' @return A diagonal matrix of group-wise sigma squared values.
+#'
+#' @examples
+#' dm <- as.dist(matrix(runif(100), nrow = 10))
+#' f <- factor(c(rep("A", 5), rep("B", 5)))
+#' dist.group.sigma2(dm, f)
+dist.group.sigma2 = function(dm, f) {
+  diag(dist.ss2(as.matrix(dm)^2, f)) / table(f) / (table(f) - 1)
 }
 
+#' Calculate Cohen's d for Distance Matrix
+#'
+#' This function calculates the Cohen's d statistic for a distance matrix,
+#' assuming two levels in the factor variable.
+#'
+#' @param dm Distance matrix.
+#' @param f Factor variable with two levels.
+#'
+#' @return Cohen's d value if factor has exactly two levels; NULL otherwise.
+#'
+#' @examples
+#' dm <- as.dist(matrix(runif(100), nrow = 10))
+#' f <- factor(c(rep("A", 5), rep("B", 5)))
+#' dist.cohen.d(dm, f)
 dist.cohen.d = function(dm, f){
   if(nlevels(f) != 2) return(NULL)
   SS2 = dist.ss2(as.matrix(dm^2),f)
--- pkg/R/test_utils.R
+++ pkg/R/test_utils.R
@@ -1,3 +1,27 @@
+#' Conducts a generic distance-based permutation test for k-group differences
+#'
+#' This function performs a distance-based permutation test using an arbitrary test statistic.
+#' It is built on the principle of Welch's ANOVA and extends it to handle multivariate distance data.
+#' It is particularly useful for analyzing microbiome data.
+#'
+#' @param test.statistic A function to calculate the test statistic.
+#' @param dm A distance or dissimilarity matrix.
+#' @param f A factor variable representing the groups.
+#' @param nrep The number of permutations to conduct. Default is 999.
+#' @param strata An optional stratifying variable for restricted permutation.
+#'
+#' @return A list containing:
+#' \itemize{
+#'   \item \code{p.value}: The p-value of the test
+#'   \item \code{statistic}: The observed test statistic
+#'   \item \code{nrep}: The number of permutations performed
+#' }
+#'
+#' @references Hamidi, Bashir, et al. "$ W_ {d}^{*} $-test: robust distance-based multivariate analysis of variance." Microbiome 7.1 (2019): 1-9.
+#' @seealso \code{\link{Tw2.test}}, \code{\link{WdS.test}}
+#' @export
+#' @examples
+#' # TODO: Add examples
 generic.distance.permutation.test =
   function(test.statistic, dm, f, nrep=999, strata = NULL){
     N = length(f)
@@ -23,10 +47,52 @@ generic.distance.permutation.test =
     list(p.value = p.value, statistic = statistic, nrep=nrep)
   }
 
+#' Conducts a Tw2 distance-based permutation test for k-group differences
+#'
+#' This function is a specialized version of the \code{\link{generic.distance.permutation.test}},
+#' specifically designed to use the Tw2 test statistic for k-group comparison.
+#'
+#' @param dm A distance or dissimilarity matrix.
+#' @param f A factor variable representing the groups.
+#' @param nrep The number of permutations to conduct. Default is 999.
+#' @param strata An optional stratifying variable for restricted permutation.
+#'
+#' @return A list containing:
+#' \itemize{
+#'   \item \code{p.value}: The p-value of the test
+#'   \item \code{statistic}: The observed test statistic
+#'   \item \code{nrep}: The number of permutations performed
+#' }
+#'
+#' @seealso \code{\link{generic.distance.permutation.test}}, \code{\link{WdS.test}}
+#' @export
+#' @examples
+#' # TODO: Add examples
 Tw2.test = function(dm, f, nrep=999, strata=NULL){
   generic.distance.permutation.test(Tw2, dm = dm, f = f, nrep = nrep, strata=strata)
 }
 
+#' Conducts a WdS distance-based permutation test for k-group differences
+#'
+#' This function is a specialized version of the \code{\link{generic.distance.permutation.test}},
+#' specifically designed to use the WdS test statistic for k-group comparison.
+#'
+#' @param dm A distance or dissimilarity matrix.
+#' @param f A factor variable representing the groups.
+#' @param nrep The number of permutations to conduct. Default is 999.
+#' @param strata An optional stratifying variable for restricted permutation.
+#'
+#' @return A list containing:
+#' \itemize{
+#'   \item \code{p.value}: The p-value of the test
+#'   \item \code{statistic}: The observed test statistic
+#'   \item \code{nrep}: The number of permutations performed
+#' }
+#'
+#' @seealso \code{\link{generic.distance.permutation.test}}, \code{\link{Tw2.test}}
+#' @export
+#' @examples
+#' # TODO: Add examples
 WdS.test = function(dm, f, nrep=999, strata=NULL){
   generic.distance.permutation.test(WdS, dm = dm, f = f, nrep = nrep, strata=strata)
 }
