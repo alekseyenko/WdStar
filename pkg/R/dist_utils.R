481800960850830ec82ab8ad7c6407c9a5f78c0c
Ivan Korostenskij <koros@Air.local>
2023-11-15 11:56:49 -0500

    add aPCoA function

--- pkg/R/dist_utils.R
+++ pkg/R/dist_utils.R
@@ -98,3 +98,70 @@ dist.cohen.d <- function(dm, f) {
 
   mean.diff / sqrt(((ns[1] - 1) * s1 + (ns[2] - 1) * s2) / (sum(ns) - 2))
 }
+
+#' Covariate Adjusted Principal Coordinates Analysis
+#'
+#' This function takes in a formula and distance matrix, adjusts for covariates,
+#' performs PCoA, and returns the resuting corrected matrix
+#'
+#' @param formula A typical formula such as Y~ A, but here Y is a dissimilarity distance. 
+#'                The formula has the same requirements as in adonis function of the vegan package.
+#'                
+#' @param data A dataset with the rownames the same as the rownames in distance.
+#'             This dataset should include both the confounding covariate and the primary covariate.
+#'
+#' @return Returns a distance matrix of class `"dist"` representing the Euclidean distances 
+#'         between the aPCoA coordinates.
+#'
+#' @details The 'aPCoA.dist' function first evaluates the left-hand side (LHS) of the formula 
+#'          within the data frame. Then, it constructs a model matrix from the right-hand side 
+#'          (RHS) of the formula. After performing necessary matrix operations and eigen 
+#'          decomposition, it calculates the Euclidean distances between the aPCoA coordinates.
+#'
+#' @examples
+#' data(iris)
+#' formula <- Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
+#' dist_matrix <- apcoa.dist(formula, iris)
+#' print(dist_matrix)
+#'
+aPCoA.dist = function (formula, data){
+  Terms <- terms(formula, data = data)
+  lhs <- formula[[2]]
+  lhs <- eval(lhs, data, parent.frame())
+  formula[[2]] <- NULL
+  rhs.frame <- model.frame(formula, data, drop.unused.levels = TRUE)
+  rhs <- model.matrix(formula, rhs.frame)
+  grps <- attr(rhs, "assign")
+  qrhs <- qr(rhs)
+  rhs <- rhs[, qrhs$pivot, drop = FALSE]
+  rhs <- rhs[, 1:qrhs$rank, drop = FALSE]
+  grps <- grps[qrhs$pivot][1:qrhs$rank]
+  u.grps <- unique(grps)
+  nterms <- length(u.grps) - 1
+  if (nterms < 1) 
+    stop("right-hand-side of formula has no usable terms")
+  dmat <- as.matrix(lhs^2)
+  X <- rhs
+  y <- lhs
+  X <- X[rownames(dmat), ]
+  X <- as.matrix(X[, -1], nrow = nrow(X))
+  H <- X %*% solve(t(X) %*% X) %*% t(X)
+  A <- -1/2 * as.matrix(y)^2
+  J <- diag(nrow(X)) - matrix(rep(1/(nrow(X)), length(A)), 
+                              nrow = nrow(A))
+  E <- (diag(nrow(H)) - H) %*% J %*% A %*% J %*% (diag(nrow(H)) - 
+                                                    H)
+  rownames(E) <- rownames(data)
+  colnames(E) <- rownames(data)
+  
+  eigen_stat = eigen(E)
+  eigenE <- eigen_stat$vectors
+  eigenvalue <- eigen_stat$values
+  
+  rownames(eigenE) <- rownames(data)
+  plotMatrix <- eigenE * matrix(rep(eigenvalue^(1/2), each = nrow(eigenE)), 
+                                nrow = nrow(eigenE))
+  plotMatrix <- plotMatrix[, !is.na(apply(plotMatrix, 2, sum))]
+  
+  dist(plotMatrix, method="euclidean")
+}
